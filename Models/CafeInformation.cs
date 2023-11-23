35bbd6030c2bb5039312f7e94e354b2df4ac56aa
Daniel Malmgren de Oliveira <danmalmx@gmail.com>
2023-11-23 14:23:13 +0100

    Fix class typo

--- Models/CafeInformation.cs
+++ Models/CafeInformation.cs
@@ -1,14 +1,18 @@
+using littlesipper_api.Models;
+
 namespace littlesipper_api;
 
 public class CafeInformation
 {
-public Guid Id { get; set; }
-public string Name { get; set; } = "";
-public string StreetAddress{ get; set; } = "";
-public string City { get; set; } = "";
-public string PostalCode { get; set; } = "";
+    public Guid Id { get; set; }
+    public string Name { get; set; } = "";
+    public string StreetAddress{ get; set; } = "";
+    public string City { get; set; } = "";
+    public string PostalCode { get; set; } = "";
 
-public string Latitude { get; set; } = "";
-public string Longitude { get; set; } = "";
+    public string Latitude { get; set; } = "";
+    public string Longitude { get; set; } = "";
+
+    public Amenities Amenities { get; set; } = new Amenities(false, false, false, false, false);
 
 }
