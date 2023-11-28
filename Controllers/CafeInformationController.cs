9c64106b77e5afa8f40758a5b44bb3968851ee57
Daniel Malmgren de Oliveira <danmalmx@gmail.com>
2023-11-28 09:14:27 +0100

    Add Post cafe

--- Controllers/CafeInformationController.cs
+++ Controllers/CafeInformationController.cs
@@ -51,73 +51,59 @@ public class CafeInformationController : ControllerBase
     [HttpGet]
     public ActionResult<List<CafeInformation>> Get()
     {
-        // var getCafeList = _context.Cafes.ToList();
-        // if (getCafeList == null)
-        // {
-        //     return NotFound("No café were found.");
-        // }
         return Ok(cafes);
     }
 
     [HttpGet("{id}")]
-    public async Task<ActionResult<CafeInformation>> Get(Guid id)
+    public ActionResult<CafeInformation> GetSingle(Guid id)
     {
-        var cafe = await _context.Cafes.FindAsync(id);
+        var cafe = cafes.FirstOrDefault(cafe => cafe.Id == id);
+        return Ok(cafe);
+    }
+
+    [HttpPost]
+    public ActionResult<List<CafeInformation>> AddCafe(CafeInformation cafe)
+    {
+        cafes.Add(cafe);
+        return Ok(cafes);
+    }
+
+    [HttpPut("{id}")]
+    public ActionResult Put(Guid id)
+    {
+
+        var cafe = cafes.FirstOrDefault(cafe => cafe.Id == id);
+
         if (cafe == null)
         {
             return NotFound($"No café with id {id} was found.");
         }
-        return cafe;
-    }
 
-    [HttpPost]
-    public async Task<ActionResult<CafeInformation>> Post(CafeInformation cafe)
-    {
-        _context.Cafes.Add(cafe);
-        await _context.SaveChangesAsync();
-        return CreatedAtAction(nameof(Get), new { id = cafe.Id }, cafe);
-    }
+        var updatedCafe  =
+            cafe.Name = cafe.Name;
+            cafe.StreetAddress = cafe.StreetAddress;
+            cafe.City = cafe.City;
+            cafe.PostalCode = cafe.PostalCode;
+            cafe.Latitude = cafe.Latitude;
+            cafe.Longitude = cafe.Longitude;
+            cafe.Amenities = cafe.Amenities;
 
-    [HttpPut("{id}")]
-    public async Task<IActionResult> Put(Guid id, CafeInformation cafe)
-    {
-        if (id != cafe.Id)
-        {
-            return BadRequest();
-        }
-
-        _context.Entry(cafe).State = EntityState.Modified;
-
-        try
-        {
-            await _context.SaveChangesAsync();
-        }
-        catch (DbUpdateConcurrencyException)
-        {
-            if (!_context.Cafes.Any(e => e.Id == id))
-            {
-                return NotFound();
-            }
-            else
-            {
-                throw;
-            }
-        }
-
-        return NoContent();
+        return Ok(cafe);
     }
 
     [HttpDelete("{id}")]
-    public async Task<IActionResult> Delete(Guid id)
+    public ActionResult Delete(Guid id)
     {
-        var cafe = await _context.Cafes.FindAsync(id);
+        var cafe = cafes.FirstOrDefault(cafe => cafe.Id == id);
+
         if (cafe == null)
         {
-            return NotFound();
+            return NotFound($"No café with id {id} was found.");
         }
 
-        _context.Cafes.Remove(cafe);
-        await _context.SaveChangesAsync();
-        return NoContent();
+            cafes.Remove(cafe);
+
+            return Ok();
+
     }
 }
